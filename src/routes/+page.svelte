d9a4302811fae52d693f45cb594739383a3319ea
arbilicious <arb2@centaurifinance.com>
2023-01-26 07:09:39 -0200

    added index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,81 @@
-<script> 
-    import WalletConnect from '$lib/WalletConnect.svelte';
-    import AddQuestion from '$lib/AddQuestion.svelte';
-    import AllQuestions from '$lib/AllQuestions.svelte';
-    import contractAbi from '../contracts/QuizFactory.json';
-    const contractAddr = '0x989C0BFE382B645e79Ab9716C28C6d71e85C6802';
 
-    export let web3Props ={
-        provider: null,
-        signer: null,
-        account: null,
-        chainId: null,
-    };
-</script>
+	<!-- Overlay Navigation Menu -->
+	<div class="overlay-navigation-wrapper" data-no-scrollbar data-animation="scale-in">
+		<div class="overlay-navigation-scroll-pane">
+			<div class="overlay-navigation-inner">
+				<div class="v-align-middle">
+					<div class="overlay-navigation-header row collapse full-width">
+						<div class="column width-12">
+							<div class="navigation-hide overlay-nav-hide">
+								<a href="#">
+									<span class="icon-cancel"></span>
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="row collapse full-width">
+						<div class="column width-12">
+							<nav class="overlay-navigation nav-block center">
+								<ul>
+									<li><a href="https://earn.mathformoney.app/what-are-the-factors-of-33">Factors of 33</a></li>
+									<li><a href="https://earn.mathformoney.app/what-are-the-factors-of-100">Factors of 100</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+					<div class="overlay-navigation-footer row full-width center">
+						<div class="column width-12 no-padding">
+							<p class="copyright">&copy; 2023 Math for Money. All Rights Reserved.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Overlay Navigation Menu End -->
 
-{#if !web3Props.account}
-    <WalletConnect bind:web3Props {contractAddr} {contractAbi}/>
-{:else}
-    <AllQuestions bind:web3Props/>
-{/if}
+			<!-- Header -->
+			<header class="header header-fixed header-fixed-on-mobile header-transparent" data-bkg-threshold="100" data-compact-threshold="100">
+				<div class="header-inner">
+					<div class="row nav-bar">
+						<div class="column width-12 nav-bar-inner">
+							<div class="logo">
+								<div class="logo-inner">
+									<a href="index"><img src ="./logo-dark.webp" alt="Math For Money Logo" /></a>
+									<a href="index"><img src ="./logo.webp" alt="Math For Money Logo" /></a>
+								</div>
+							</div>
+							<nav class="navigation nav-block secondary-navigation nav-right">
+								<ul>
+									<li>
+										<!-- Dropdown Login -->
+										<div class="v-align-middle">
+											<a href="https://earn.mathformoney.app/what-are-the-factors-of-33" class="button no-page-fade no-label-on-mobile small no-margin-bottom"><span class="icon-lock left"></span><span>Factors 33</span></a>
+											<a href="https://earn.mathformoney.app/what-are-the-factors-of-100" class="button no-page-fade no-label-on-mobile small no-margin-bottom"><span class="icon-lock left"></span><span>Factors 100</span></a>
+
+                                        </div>
+									</li>
+									<li class="aux-navigation hide">
+										
+			                            <!-- Aux Navigation -->
+										<a href="#" class="navigation-show overlay-nav-show nav-icon">
+											<span class="icon-menu"></span>
+										</a>
+									</li>
+								</ul>
+							</nav>
+							<nav class="navigation nav-block primary-navigation nav-right">
+								<ul>
+									<li >
+									<li><a href="https://earn.mathformoney.app/what-are-the-factors-of-33">Factors of 33</a></li>
+									<li><a href="https://earn.mathformoney.app/what-are-the-factors-of-100">Factors of 100</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</header>
+			<!-- Header End -->
+
+
 
