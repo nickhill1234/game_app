f3ee97aa7b109811f3b73ee6ae739f22e23885f9
arbilicious <arb2@centaurifinance.com>
2023-02-20 06:40:36 +0200

    updated URL for sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -18,85 +18,85 @@ export async function GET({ setHeaders }) {
     >      
     
     <url>
-      <loc>https://www.moneymathgames.com/.app/what-are-the-factors-of-33</loc>
-      <lastmod>2023-01-28</lastmod>
+      <loc>https://moneymathgames.com/what-are-the-factors-of-33</loc>
+      <lastmod>2023-02-20</lastmod>
       <changefreq>daily</changefreq>
       <priority>1.0</priority>
     </url>
 
     <url>
-      <loc>https://www.moneymathgames.com/what-are-the-factors-of-100</loc>
-      <lastmod>2023-01-28</lastmod>
+      <loc>https://moneymathgames.com/what-are-the-factors-of-100</loc>
+      <lastmod>2023-02-20</lastmod>
       <changefreq>daily</changefreq>
       <priority>1.0</priority>
     </url>
 
     <url>
-      <loc>https://www.moneymathgames.com/what-decimal-is-1:6</loc>
-      <lastmod>2023-02-02</lastmod>
+      <loc>https://moneymathgames.com/what-decimal-is-1:6</loc>
+      <lastmod>2023-02-20</lastmod>
       <changefreq>daily</changefreq>
       <priority>1.0</priority>
     </url>
 
     <url>
-      <loc>https://www.moneymathgames.com/what-decimal-is-5:6</loc>
-      <lastmod>2023-02-02</lastmod>
+      <loc>https://moneymathgames.com/what-decimal-is-5:6</loc>
+      <lastmod>2023-02-20</lastmod>
       <changefreq>daily</changefreq>
       <priority>1.0</priority>
     </url>    
 
     <url>
-    <loc>https://www.moneymathgames.com/5-is-what-percent-of-2</loc>
-    <lastmod>2023-02-02</lastmod>
+    <loc>https://moneymathgames.com/5-is-what-percent-of-2</loc>
+    <lastmod>2023-02-20</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
     </url>    
 
     <url>
-    <loc>https://www.moneymathgames.com/5-is-what-percent-of-20</loc>
-    <lastmod>2023-02-02</lastmod>
+    <loc>https://moneymathgames.com/5-is-what-percent-of-20</loc>
+    <lastmod>2023-02-20</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
     </url> 
 
     <url>
-    <loc>https://www.moneymathgames.com/what-percentage-of-8-is-5</loc>
-    <lastmod>2023-02-02</lastmod>
+    <loc>https://moneymathgames.com/what-percentage-of-8-is-5</loc>
+    <lastmod>2023-02-20</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
     </url> 
 
     <url>
-    <loc>https://www.moneymathgames.com/what-percentage-of-20-is-5</loc>
-    <lastmod>2023-02-02</lastmod>
+    <loc>https://moneymathgames.com/what-percentage-of-20-is-5</loc>
+    <lastmod>2023-02-20</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
     </url> 
 
     <url>
-    <loc>https://www.moneymathgames.com/3-fifteens</loc>
-    <lastmod>2023-02-02</lastmod>
+    <loc>https://moneymathgames.com/3-fifteens</loc>
+    <lastmod>2023-02-20</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
     </url> 
 
     <url>
-    <loc>https://www.moneymathgames.com/5-1:2-as-a-decimal</loc>
-    <lastmod>2023-02-02</lastmod>
+    <loc>https://moneymathgames.com/5-1:2-as-a-decimal</loc>
+    <lastmod>2023-02-20</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
     </url> 
 
     <url>
-    <loc>https://www.moneymathgames.com/0.75-as-a-fraction</loc>
-    <lastmod>2023-02-02</lastmod>
+    <loc>https://moneymathgames.com/0.75-as-a-fraction</loc>
+    <lastmod>2023-02-20</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
     </url> 
 
     <url>
-    <loc>https://www.moneymathgames.com/0.125-as-a-fraction</loc>
-    <lastmod>2023-02-02</lastmod>
+    <loc>https://moneymathgames.com/0.125-as-a-fraction</loc>
+    <lastmod>2023-02-20</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
     </url> 
